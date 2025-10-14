3f237e60f8880675e7b8afbab6219e786b80011f
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-14 23:35:26 +0000

    upload de arquivo

---
+++ exercicios_print_python/ex_10.py
@@ -0,0 +1,6 @@
+#Escreva um programa que mostre a tabuada do número 2(de 2x1 ate 2x10)usando vários prints
+
+
+print('--- Tabuada do 2 ---')
+for i in range(1,11):
+    print(f' 2 x {i} = {i * 2}')
