4dbbfbec8faab3ac5be87678405fe7a307fcbc0d
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:23:55 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_28.py
@@ -0,0 +1,5 @@
+#Peça ao usuário uma cidade e um ano e mostre: 'Visitei [cidade] em [ano].
+
+cidade = input('Insira uma cidade: ')
+ano = input('Insira um ano: ')
+print(f'Visitei {cidade} em {ano}')
