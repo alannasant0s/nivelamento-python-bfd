7aaf431fb6e8aec620548ba61086275b956256b5
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:22:13 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_27.py
@@ -0,0 +1,5 @@
+#Peça o nome de um cantor(a) e uma música e imprima: '[cantor] canta a música [musica]
+
+cantor = input('Insira o nome de um cantor: ')
+musica = input('Insira o nome de uma música: ')
+print(f'{cantor} canta a música {musica}')
