330286e7f21355db9d300f31809cf834456f6e2f
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:23:41 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_07.py
@@ -0,0 +1,6 @@
+#Peça dois números e mostre a subtração
+
+escolha1 = int(input('Insira um número: '))
+escolha2 = int(input('Insira um número: '))
+
+print(f'{escolha1} - {escolha2} = {escolha1 - escolha2}')
