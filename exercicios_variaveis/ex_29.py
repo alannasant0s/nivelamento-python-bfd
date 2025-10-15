0f04978247c6b47b0b807b044f572b23ab3a0299
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:25:56 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_29.py
@@ -0,0 +1,5 @@
+#Peça o nome de uma escola e um curso e mostre: 'Estudo [curso] na escola [escola]
+
+escola = input('Insira o nome da escola: ')
+curso = input('Insira o curso: ')
+print(f'Estudo {curso} na escola {escola}')
