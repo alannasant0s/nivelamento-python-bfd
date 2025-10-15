d141020a43c66d9f96c7d071097c191c72aca122
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:40:00 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_09.py
@@ -0,0 +1,6 @@
+#Peça dois números e mostre a divisão
+
+escolha1 = int(input('Insira um número maior que 0: '))
+escolha2 = int(input('Insira um número: '))
+
+print(f'{escolha1} / {escolha2} = {escolha1 / escolha2}')
