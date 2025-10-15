a747e2a3b76b7f1542924301f8cd21d3b136fb10
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 01:27:52 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_15.py
@@ -0,0 +1,5 @@
+#Peça o valor de um produto e mostre com 10% de desconto
+
+valor_produto = float(input('Digite o valor do produto para que possamos calcular o seu desconto: '))
+
+print(f'O valor do seu produto com desconto é: {valor_produto - (0.1 * valor_produto)}')
