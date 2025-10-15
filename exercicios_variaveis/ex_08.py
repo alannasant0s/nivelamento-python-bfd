d3fabf8ac3eab6e0dea55d048ebe5056cbe1d22d
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:25:31 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_08.py
@@ -0,0 +1,5 @@
+#Peça dois números e mostre a multiplicação.
+
+escolha1 = int(input('Insira um número: '))
+escolha2 = int(input('Insira um número: '))
+print(f'{escolha1} x {escolha2} = {escolha1 * escolha2}')
