6e41d26b45b6ae979c6cd0b54655cfcbd016a1c5
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:28:09 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_30.py
@@ -0,0 +1,5 @@
+#Peça ao usuário o nome de um amigo e um apelido e mostre: 'Meu amigo [nome] também é conhecido como [apelido].
+
+amigo = input('Insira o nome do seu amigo: ')
+apelido = input('Insira o apelido do seu amigo: ')
+print(f'Meu amigo {amigo} também é conhecido como {apelido}')
