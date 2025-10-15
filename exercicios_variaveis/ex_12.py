9a63ef98bd5866a0549bfd1ae8fb292ed0e8a995
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:54:07 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_12.py
@@ -0,0 +1,6 @@
+#Crie uma variável ano_atual e ano_nascimento, calcule a idade e imprima.
+
+ano_atual = int(input('Insira o ano atual: '))
+ano_nascimento = int(input('Insira o ano do seu nascimento: '))
+
+print(f'A sua idade é: {ano_atual - ano_nascimento}')
