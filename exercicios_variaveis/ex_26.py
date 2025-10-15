76af416888bac0eaece573575bdc2bbe20b33d2a
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:19:50 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_26.py
@@ -0,0 +1,6 @@
+#Peça o nome do usuário e uma comida favorita e imprima: '[nome] gosta de comer [comida].
+
+nome = input('Insira o seu nome: ')
+comida_favorita = input('Insira sua comida fav: ')
+
+print(f'{nome} gosta de comer {comida_favorita}')
