b1a4af6e9a9c57f01e2beb578a5523beb3ef9d81
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:11:24 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_02.py
@@ -0,0 +1,6 @@
+#Peça ao usuário seu nome e idade e imprima: 'Olá [nome], você tem [idade] anos.
+
+nome = input('Por favor, insira o seu nome: ')
+idade = int(input('Insira a sua idade: '))
+
+print(f'Olá {nome}, você tem {idade} anos.')
