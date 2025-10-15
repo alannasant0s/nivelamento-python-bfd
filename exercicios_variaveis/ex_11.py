272fb4384be0992fb826100eacba23c49ae4da7f
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:51:47 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_11.py
@@ -0,0 +1,7 @@
+#Peça ao usuário 3 notas e calcule a média
+
+nota1 = float(input('Insira a primeira nota: '))
+nota2 = float(input('Insira a segunda nota: '))
+nota3 = float(input('Insira a terceira nota: '))
+
+print(f'A média das suas notas é: {(nota1 + nota2 + nota3) / 3:.2f}')5
