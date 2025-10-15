10d0716f00748609dad8ae9236c7f5b54ba99da3
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 00:20:43 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_06.py
@@ -0,0 +1,6 @@
+#Peça dois números inteiros e mostre a soma
+
+escolha1 = int(input('Insira um número: '))
+escolha2 = int(input('Insira um número: '))
+
+print(f'A soma de {escolha1} + {escolha2} é: {escolha1 + escolha2}')
