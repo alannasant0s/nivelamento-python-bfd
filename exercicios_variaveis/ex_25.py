f870055423166546260ae97c68704ce4e1fca30e
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:16:55 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_25.py
@@ -0,0 +1,6 @@
+#Crie variáveis animal e nome e mostre: 'Meu animal de estimação é [animal] e se chama[nome].'
+
+animal = input('Insira o tipo de animal que você possui: ')
+nome = input('Insira o nome do animal: ')
+
+print(f'Meu animal de estimação é um {animal} e se chama {nome}')
