2d6e6dbe952a672a049b17315a56c59a256d4890
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 01:53:55 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_18.py
@@ -0,0 +1,7 @@
+#Peça 3 números e mostre a soma deles
+
+escolha1 = int(input('Insira o primeiro número: '))
+escolha2 = int(input('Insira o segundo número: '))
+escolha3 = int(input('Insira o terceiro número: '))
+
+print(f'A soma de {escolha1} + {escolha2} + {escolha3} é {escolha1 + escolha2 + escolha3}')10
