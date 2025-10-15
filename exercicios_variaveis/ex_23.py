7535a9436ed09fbf7a085f6c88fb99674791fa5a
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:10:14 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_23.py
@@ -0,0 +1,5 @@
+#Crie variáveis profissao e empresa e imprima: 'Trabalho como [profissao] na [empresa]
+
+profissao = input('Insira a sua profissão: ')
+empresa = input('Insira a empresa: ')
+print(f'Trabalho como {profissao} na {empresa}')
