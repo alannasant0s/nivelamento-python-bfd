d707bd3f623af1e4acdffdf5125d6404acc81765
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-15 02:04:52 +0000

    upload de arquivo

---
+++ exercicios_variaveis/ex_21.py
@@ -0,0 +1,6 @@
+# Crie variáveis time e jogador e imprima: '[jogador] joga no [time].
+
+time = input('Insira o nome do seu time: ')
+jogador = input('Insira o nome do jogador: ')
+
+print(f'O jogador {jogador} joga no {time}')
