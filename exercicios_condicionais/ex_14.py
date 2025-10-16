b8b142f2dea30fa8048ea29d18debe7daf4b1c99
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-16 19:51:33 +0000

    Upload de exercicio

---
+++ exercicios_condicionais/ex_14.py
@@ -0,0 +1,9 @@
+#Iguais ou diferentes
+
+a = int(input('Insira um número: '))
+b = int(input('Insira um número: '))
+
+if a == b:
+    print(f'O número de a {a} é igual a b {b}')
+else: 
+    print(f'o número de a {a} é diferente do número de b {b}')
