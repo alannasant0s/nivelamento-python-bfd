7424566f6d5e88d7909e0db401ced52278b65a72
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-16 19:36:43 +0000

    Upload de exercicio

---
+++ exercicios_condicionais/ex_13.py
@@ -0,0 +1,7 @@
+#Número entre 1 e 100
+
+numero = int(input('Insira um número: '))
+if numero > 0 and numero <= 100:
+    print(f'O número {numero} está contido no intervalo de 1 a 100')
+else: 
+    print(f'O número {numero} não está no intervalo de 0 a 100')
