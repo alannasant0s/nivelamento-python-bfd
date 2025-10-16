1cff4f3ec67c7bf8318c20682efda4ca406294b6
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-16 18:52:39 +0000

    Upload de exercicio

---
+++ exercicios_condicionais/ex_10.py
@@ -0,0 +1,6 @@
+#Temperatura
+
+
+c = int(input('Insira a temperatura em Celsios para ser convertida em Fahrenheits: '))
+print(f'A temperatura {c} graus Celsios corresponde a {(c * 1.8) + 32} graus em Fahrenheit')
+
