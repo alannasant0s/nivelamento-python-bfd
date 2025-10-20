d621e55a2eac1c72fc7e81aaf5c99025773ee6c8
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-20 23:06:42 +0000

    upload de exercicio

---
+++ exercicios_classes/orientacao_objetos.py
@@ -0,0 +1,32 @@
+#Classes -> Representação
+#Atributos -> Variáveis
+#Métodos -> Funções
+
+
+#carro
+    #Modelo -> Ferrari
+    #Ano -> 2020
+    #Estado -> seminovo
+
+    # modelo, ano, estado, -> Atributos
+
+    #ligar()
+    #acelerar()
+    #frear()
+    #Comprar()
+    #teste_driver()
+
+#ligar(), acelerar(), frear(), Comprar(), teste_driver() -> Métodos
+
+#carro
+    #Modelo -> BMW
+    #Ano -> 2020
+    #Estado -> seminovo
+
+     #ligar()
+    #acelerar()
+    #frear()
+    #Comprar()
+    #teste_driver()
+
+#ligar(), acelerar(), frear(), Comprar(), teste_driver() -> Métodos
