41d1d13074c7f56102ff33e53e87a01b3dd7b812
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-20 23:27:05 +0000

    upload de exercicios

--- exercicios_classes/orientacao_objetos.py
+++ exercicios_classes/orientacao_objetos.py
@@ -30,3 +30,18 @@
     #teste_driver()
 
 #ligar(), acelerar(), frear(), Comprar(), teste_driver() -> Métodos
+
+class Carro(object):
+    estado = 'novo'
+
+fusca = Carro()
+fusca.estado = 'novo'
+
+ferrari = Carro()
+ferrari.estado = 'usado'
+
+print(fusca.estado) # valor do atributo estado do objeto
+print(ferrari.estado) # valor do atributo estado do objeto
+print(type(fusca)) # representação do objeto fusca
+print(ferrari) #Representação do objeto ferrari
+
