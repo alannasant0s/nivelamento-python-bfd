6a025db304cabaef1f023021701c2ae46197953a
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-22 13:36:51 +0000

    upload de exercicio

--- exercicios_classes/ex_03.py
+++ exercicios_classes/ex_03.py
@@ -1,11 +1,14 @@
 class Carro():
-    def __init__(self, marca, estado):
+    def __init__(self, marca, estado, cor):
         self.marca = marca
         self.estado = estado
+        self.cor = cor
 
-carro1 = Carro('Gol', 'Novo')
-carro2 = Carro('Vectra', 'Usado')
+carro1 = Carro('Gol', 'Novo', 'Azul')
+carro2 = Carro('Vectra', 'Usado', 'Verde')
 
-print(f'O primeiro carro é um {carro1.marca} e sua situação é {carro1.estado}')
-print(f'O segundo carro é um {carro2.marca} e sua situação é {carro2.estado}')
+print(f'O primeiro carro é um {carro1.marca} da cor {carro1.cor} e sua situação é {carro1.estado}')
+print(f'O segundo carro é um {carro2.marca} da cor {carro2.cor} e sua situação é {carro2.estado}')
+
+
 
