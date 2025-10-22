2b91481da684a98a66279c5c6ae350c7c18a79fc
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-22 13:16:14 +0000

    Upload de exercicio

---
+++ exercicios_classes/ex_02.py
@@ -0,0 +1,11 @@
+class Animal():
+    def __init__(self, especie):
+            self.especie = especie
+
+animal1 = Animal('cachorro')
+animal2 = Animal('gato')
+
+print(f'O primeiro animal é um {animal1.especie} e o segundo é {animal2.especie}')
+
+
+
