5ea72f843196485b77d97d1927024d154633cd60
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-07 00:54:56 +0000

    upload de exercicio

---
+++ exercicios_classes/ex12.py
@@ -0,0 +1,13 @@
+"""Crie uma classe Cachorro com o método latir() que imprime "Au Au!". 
+Crie um objeto e chame o método."""
+
+class Cachorro:
+
+    def __init__(self):
+        pass
+    
+    def latir(self):
+        print("au au")
+
+cachorro = Cachorro()
+cachorro.latir()
