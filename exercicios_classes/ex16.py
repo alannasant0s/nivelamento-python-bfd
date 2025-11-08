4b5dc41c286c2c09be3ee84bc2bc13c604daa577
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-08 19:13:40 +0000

    upload de exercicio

--- exercicios_classes/ex16.py
+++ exercicios_classes/ex16.py
@@ -5,13 +5,17 @@ Teste com dois depósitos diferentes"""
 
 class Conta:
     def __init__(self, saldo = 0):
-        self.saldo = saldo
+        self.saldo = float(saldo)
         
 
     def depositar(self):
-        self.saldo = int(input("Insira o valor a ser depositado: "))
+        valor = float(input("Insira o valor a ser depositado: "))
+        if valor > 0:
+            self.saldo += valor
+        else:
+            print("Valor inválido, digite um valor válido")
 
 cliente1 = Conta()
 print(f"O saldo do cliente1 é: {cliente1.saldo}")
-cliente1.depositar()
+cliente1.depositar ()
 print(f"O saldo do cliente1 é: {cliente1.saldo}")
