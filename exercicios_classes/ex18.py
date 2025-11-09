08e9425cdcfe617f5fe248f27f6dfac2d7123040
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-09 12:04:37 +0000

    upload de exercicio

---
+++ exercicios_classes/ex18.py
@@ -0,0 +1,15 @@
+"""Crie uma classe Pessoa com método falar(). 
+Ao chamar, o método deve imprimir "Olá, tudo bem?". """
+
+
+
+class Pessoa:
+    def __init__(self):
+        pass
+
+    def falar():
+        interacao = input("Qual o seu nome? ")
+        print(f"Olá {interacao}, tudo bem?")
+
+pessoa1 = Pessoa
+pessoa1.falar()
