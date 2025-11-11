aa677eadb91ee2ea034b0a27b3f1e63551275847
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-11 13:46:34 +0000

    upload de exercicio

---
+++ exercicios_classes/ex23.py
@@ -0,0 +1,19 @@
+"""Crie uma classe Pessoa e adicione um atributo idade. 
+Crie um método maior_de_idade() que retorna True se idade ≥ 18."""
+
+
+class Pessoa:
+    def __init__(self, idade):
+        self.idade = idade
+
+    
+    def maior_idade(self):
+        if self.idade >= 18:
+            return True
+        else:
+            return False
+
+idade = int(input("Insira a sua idade: "))
+pessoa1 = Pessoa(idade)
+
+print(pessoa1.maior_idade())
