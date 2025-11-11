b85b3771e0308068e8fd5423cd5e17637dd891e7
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-11 16:00:52 +0000

    chore(exercicios-python): Adiciona solução do exercício 27

---
+++ exercicios_classes/ex27.py
@@ -0,0 +1,11 @@
+"""Crie uma classe Pessoa com método __init__ que mostra uma mensagem “Pessoa 
+criada”. 
+Crie dois objetos e veja o resultado."""
+
+class Pessoa:
+    def __init__(self):
+        print("Pessoa criada")
+
+
+pessoa1 = Pessoa()
+pessoa2 = Pessoa()
