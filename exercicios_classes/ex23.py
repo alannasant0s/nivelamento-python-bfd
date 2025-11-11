6a23ef0bd5e93c1670437a0817e493594c70887c
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-11 13:59:30 +0000

    chore(exercicios-python): Adiciona solução do exercício 23

--- exercicios_classes/ex23.py
+++ exercicios_classes/ex23.py
@@ -5,7 +5,6 @@ Crie um método maior_de_idade() que retorna True se idade ≥ 18."""
 class Pessoa:
     def __init__(self, idade):
         self.idade = idade
-
     
     def maior_idade(self):
         if self.idade >= 18:
@@ -15,5 +14,4 @@ class Pessoa:
 
 idade = int(input("Insira a sua idade: "))
 pessoa1 = Pessoa(idade)
-
 print(pessoa1.maior_idade())
