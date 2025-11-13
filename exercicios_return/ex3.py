7d14042eeca2fb60fc07e7be5f8fc29660ab8571
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-13 12:53:27 +0000

    feat: Responde exercicio 3 da lista

---
+++ exercicios_return/ex3.py
@@ -0,0 +1,11 @@
+"""Crie uma função maior(a, b) que retorne o maior entre dois valores"""
+
+def maior(a,b):
+    if a > b: 
+        return a
+    else:
+        return b
+
+
+maior_numero = maior(5,9)
+print(maior_numero)
