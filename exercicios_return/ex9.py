815ca6bb38084d4f9964c49d9d42edc82d77cdc8
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-11-14 12:25:43 +0000

    Resolve o exercicio 9 da lista

---
+++ exercicios_return/ex9.py
@@ -0,0 +1,7 @@
+"""Crie uma função resto_divisao(a, b) que retorne o resto da divisão entre dois números"""
+
+def resto_divisao(a,b):
+    return a % b
+
+resto = resto_divisao(83, 5)
+print(f"O resto da divisão entre a e b é {resto}")
